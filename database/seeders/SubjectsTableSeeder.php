d183ab3f3d53a8708b7523efaa44cea4f1abc31b
ktakahashi01 <k.takahashi01@lull-inc.co.jp>
2026-03-01 18:26:08 +0900

    環境構築 初期データの登録

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -1,9 +1,9 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Users\Subjects;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -12,8 +12,35 @@ class SubjectsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run()//Eloquentで記述
     {
-        // 国語、数学、英語を追加
+        Subjects::create([
+            'subject' => '国語',
+        ]);
+
+        Subjects::create([
+            'subject' => '数学',
+        ]);
+
+        Subjects::create([
+            'subject' => '英語',
+        ]);
     }
 }
+
+//ほかの書き方
+//①insert()を使う（モデルイベントが発火しない）
+//Subject::insert([
+//     ['subject' => '国語'],
+//     ['subject' => '数学'],
+//     ['subject' => '英語'],
+// ]);
+
+//②foreachで入れる（モデルイベントが発火する）
+// $subjects = ['国語', '数学', '英語'];
+
+// foreach ($subjects as $subject) {
+//     Subject::create([
+//         'subject' => $subject,
+//     ]);
+// }
